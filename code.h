62f805bc1911cf1ecd0d607e5f16752c562b4c0b
Zachary Neveu <Zachary Neveu>
2017-01-25 13:37:19 -0500

    Add function descriptions to code.h

--- code.h
+++ code.h
@@ -20,26 +20,35 @@ private:
     int n; //n is number of digits in code
     int m; //m is maximum value of each digit
 
-    //Called by constructor, not accessible by user.
+	// randInit takes the code length n, and maximum digit value m as inputs and
+	// generates a code within these parameters.
+    // randInit() Called by constructor, not accessible by user.
     void randInit();
 public:
 
-    //simple function to return size of a codeVector.styl
+    //simple function to return size of a codeVector.
     int getLength()
     {
         return codeVector.size();
     };
 
-    //Constructor used for secret code
+	//Length and Range Constructor calls randInit to generate a random
+	//code. This constructor used for secret code.
     code(int codeLen, int MaxValue);
 
-    //Constructor used for guess code when codeVector is known.
+	//  Vector Constructor takes vector as input and passes it to the new code
+	//  object as its code member. If digits of vector are out of range, user
+	//  is notified and program continues.
+    // Constructor used for guess code when codeVector is known.
     code(const vector<int> &setVector, int MaxValue);
 
-    //function to check how many digits are correct
+	// checkCorrect returns how many digits in the guess match the digits in
+	// the code both in value and in position.  Check Correct also validates
+	// that all digits of the input are within the specified range
     const int checkCorrect(const code &guess);   //Const so cannot edit code
 
-    //function to check how many digits are correct but in the wrong place
+	// checkIncorrect returns the number of digits in the guess that are in the
+	//code but in the wrong position.
     const int checkIncorrect(const code &guess); //Const so cannot edit code
 };
 
