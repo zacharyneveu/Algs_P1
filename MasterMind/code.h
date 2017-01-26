adaba4b52cfaa5879175dc7020b0be8318c1630e
Jake Dec <jakedec@gmail.com>
2017-01-26 10:51:43 -0500

    Changed to VS Project
    Still needs to be cleaned up

---
+++ MasterMind/code.h
@@ -0,0 +1,56 @@
+// Project #1
+// Zachary Neveu, neveu.z@husky.neu.edu
+// Jake Dec, dec.j@husky.neu.edu.
+//
+// This file contains the definition of the code class.  Implementations for
+// the member functions of this class can be found in code.cpp
+
+#ifndef CODE_H
+#define CODE_H
+
+#include<vector>
+
+using namespace std; //using standard name space
+
+class code
+{
+private:
+	vector<int> codeVector; //Data member of the class
+
+	int n; //n is number of digits in code
+	int m; //m is maximum value of each digit
+
+		   // randInit takes the code length n, and maximum digit value m as inputs and
+		   // generates a code within these parameters.
+		   // randInit() Called by constructor, not accessible by user.
+	void randInit();
+public:
+
+	//simple function to return size of a codeVector.
+	int getLength()
+	{
+		return codeVector.size();
+	};
+
+	//Length and Range Constructor calls randInit to generate a random
+	//code. This constructor used for secret code.
+	code(int codeLen, int MaxValue);
+
+	//  Vector Constructor takes vector as input and passes it to the new code
+	//  object as its code member. If digits of vector are out of range, user
+	//  is notified and program continues.
+	// Constructor used for guess code when codeVector is known.
+	code(const vector<int> &setVector, int MaxValue);
+
+	// checkCorrect returns how many digits in the guess match the digits in
+	// the code both in value and in position.  Check Correct also validates
+	// that all digits of the input are within the specified range
+	const int checkCorrect(const code &guess);   //Const so cannot edit code
+
+												 // checkIncorrect returns the number of digits in the guess that are in the
+												 //code but in the wrong position.
+	const int checkIncorrect(const code &guess); //Const so cannot edit code
+};
+
+#endif
+
--- README.md
+++
@@ -1,3 +0,0 @@
-Authors: Zachary Neveu & Jake Dec
-
-The purpose of this repository is to store code for project 1 from fundamentals of engineering algorithms at NEU.
