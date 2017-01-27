e7ed03d061cc87859e8fc81ae7e7b4287616cce9
Jake Dec <jakedec@gmail.com>
2017-01-27 10:28:02 -0500

    Updated main

--- MasterMind/main.cpp
+++ MasterMind/main.cpp
@@ -20,66 +20,17 @@ void getCodeValues(int &n, int &m);
 
 int main()
 {
-	//Get inputs for Random Generator
-	int length = 5;
-	int	MaxValue = 10; //n=code length, m=max digit value
+	int n, m; //code length and range
 
-					   //initialize the code to be guessed within the specified parameters
-	code secretCode(length, MaxValue);
+	//get the desired code values from the user
+	getCodeValues(n, m); 
 
-	//Vector to store guess
-	vector<int> guessVector;
+	//create the mastermind object
+	mastermind game(n, m);
 
-	for (int i = 0; i < 3; i++)
-	{
-		if (i == 0)
-		{
-			int guessArray1[5] = { 5, 0, 3, 2, 6 };
-			vector<int> guess1Vector(guessArray1, guessArray1 + 5);
-			guessVector = guess1Vector;
-			cout << "Guess 1: {5, 0, 3, 2, 6}" << endl;
-		}
-		else if (i == 1)
-		{
-			int guessArray2[5] = { 2, 1, 2, 2, 2 };
-			vector<int> guess2Vector(guessArray2, guessArray2 + 5);
-			guessVector = guess2Vector;
-			cout << "Guess 2: {2, 1, 2, 2, 2}" << endl;
-		}
-		else
-		{
-			int guessArray3[5] = { 1, 3, 3, 4, 5 };
-			vector<int> guess3Vector(guessArray3, guessArray3 + 5);
-			guessVector = guess3Vector;
-			cout << "Guess 3: {1, 3, 3, 4, 5}" << endl;
-		}
+	//start the game, print the code
+	game.playGame();
 
-
-		//initialize code object for guess vector
-		code guess(guessVector, MaxValue);
-
-		//Store  number correct as int so only one call needed
-		int correct = secretCode.checkCorrect(guess);
-
-		//If answer not completely correct
-		if (correct < secretCode.getLength())
-		{
-			//print number of correct digits
-			cout << "Correct: " << correct << endl;
-			//Print number of incorrect digits
-			cout << "Incorrect: " << secretCode.checkIncorrect(guess) << endl;
-		}
-
-		else //correct==n && tries<10
-		{
-			cout << "You Win!!" << endl;
-			break;//End game
-		}
-	}//End for loop for three guesses
-
-
-
-	 //exit program
 	return 0;
 }//End Main Function
 
